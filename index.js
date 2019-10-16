479a231abf725d8532b560d550a55b60ace12f88
kumkumsingh <kumkumsingh.r@gmail.com>
2019-10-16 17:57:56 +0200

    added model for creating table

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const express = require('express')
-const db = require('./db')
+const express = require('express');
+const model = require('./team/model');
+const db = require('./db');
 const app = express()
 const port = process.env.PORT || 4000
 
