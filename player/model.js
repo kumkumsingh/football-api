4509a6a36102574fbe6fe49f63bc949ce7c9d5f7
kumkumsingh <kumkumsingh.r@gmail.com>
2019-10-23 22:32:34 +0200

    added cascade for deleting teams deleting player as well

--- player/model.js
+++ player/model.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize')
 const Team = require('../team/model')
 const db = require('../db')
 const Player = db.define('player',{name:{type:Sequelize.STRING},number:{type:Sequelize.INTEGER}})
-Player.belongsTo(Team)
+Player.belongsTo(Team,{onDelete:'CASCADE'})
 Team.hasMany(Player)
 
 module.exports = Player
